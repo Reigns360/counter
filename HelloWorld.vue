bbe1c98a51c01988080395c66c37576d26f01347
Reigns360 <128800522+Reigns360@users.noreply.github.com>
2024-02-23 07:30:15 -0800

    Add files via upload

---
+++ HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="counter">
+    <v-btn color="primary" @click="decrement" class="mr-2">-</v-btn>
+    <span class="count">{{ count }}</span>
+    <v-btn color="primary" @click="increment" class="ml-2">+</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    decrement() {
+      if (this.count > 0) {
+        this.count--;
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.counter {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.count {
+  font-size: 2em;
+  margin: 0 20px;
+}
+</style>
